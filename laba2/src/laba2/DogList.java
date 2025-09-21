e4448d9e70be2cfb5f8678c97fccf706c7a8aed2
Vedmix <vedmix.work@gmail.com>
2025-09-21 23:09:06 +0300

    added cool buttons

--- laba2/src/laba2/DogList.java
+++ laba2/src/laba2/DogList.java
@@ -13,6 +13,7 @@ import java.awt.Font;
 import java.awt.GridLayout;
 import java.io.*;
 import javax.swing.*;
+import java.awt.Image;
 
 public class DogList 
 {
@@ -20,37 +21,59 @@ public class DogList
 	{
 		FileManager fm = new FileManager();
 		List<Dog> dogs = new List<>();
+		
 		dogs = fm.inputFromCSV("src/data/dogs.csv");
 		dogs.print();
 		fm.outputToCSV("src/data/dogs.csv", dogs);
-		
-		JFrame frame = new JFrame("Dog List");
-		
+		 
+		JFrame frame = new JFrame("Dog Festival");
 		ImageIcon icon = new ImageIcon("src/laba2/dogIcon.png");
 		frame.setIconImage(icon.getImage());
-		
+
 		JPanel buttonsPanel = new JPanel();
-		buttonsPanel.setLayout(new GridLayout(6, 1, 10, 10));
-		
-		JButton[] buttons = {
-		new JButton("Pull"),
-		new JButton("Push"),
-		new JButton("Print"),
-		new JButton("Add"),
-		new JButton("Search"),
-		new JButton("Remove")
+		buttonsPanel.setLayout(new GridLayout(1, 7, 10, 10));
+
+		String[] imagePaths = {
+		    "src/laba2/save.png",
+		    "src/laba2/folder_documents.png",
+		    "src/laba2/cloud.png",
+		    "src/laba2/plus.png",
+		    "src/laba2/minus.png",
+		    "src/laba2/edit.png",
+		    "src/laba2/print.png" 
 		};
-		
-		for(JButton button : buttons)
-		{
-			buttonsPanel.add(button);
-			button.setFont(new Font("Arial", Font.PLAIN, 30));
+
+		String[] tooltips = {
+		    "Save",
+		    "Open",
+		    "Backup",
+		    "Add",
+		    "Remove",
+		    "Edit",
+		    "Print"
+		};
+
+		JButton[] buttons = new JButton[7];
+
+		for(int i = 0; i < 7; i++) {
+		    ImageIcon buttonIcon = new ImageIcon(imagePaths[i]);
+		    
+		    Image scaledImage = buttonIcon.getImage().getScaledInstance(32, 32, Image.SCALE_SMOOTH);
+		    ImageIcon scaledIcon = new ImageIcon(scaledImage);
+		    
+		    buttons[i] = new JButton(scaledIcon);
+		    buttons[i].setToolTipText(tooltips[i]); 
+	
+		    buttons[i].setBorderPainted(false);
+		    buttons[i].setContentAreaFilled(false);
+		    buttons[i].setFocusPainted(false);
+		    
+		    buttonsPanel.add(buttons[i]);
 		}
-		
-		frame.getContentPane().add(BorderLayout.CENTER, buttonsPanel);
+
+		frame.getContentPane().add(BorderLayout.NORTH, buttonsPanel);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(700, 600); 
 		frame.setVisible(true);
-		
-		frame.setSize(500, 600);
-		
 	}
 }
